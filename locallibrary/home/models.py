76d7c146503505dd9381ae876303dcbb10da6648
itaykbn <itay.kbn@gmail.com>
2022-01-30 10:50:58 +0200

    start to add database

--- locallibrary/home/models.py
+++ locallibrary/home/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import date
+
+from django.core.files.storage import FileSystemStorage
+
+
+class Author(models.Model):
+    user_name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user_name
+
+
+image_store = FileSystemStorage(location='/tmp/images')
+
+
+class Entry(models.Model):
+    username = models.TextField(Author)
+
+    headline = models.CharField(max_length=255)
+    image_path = models.ImageField(storage=image_store)
+    body_text = models.TextField()
+
+    pub_date = models.DateField()
+    mod_date = models.DateField(default=date.today)
+
+    commentors = models.ManyToManyField(Author)
+
+    number_of_comments = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.headline
 
 # Create your models here.
--- locallibrary/home/views.py
+++ locallibrary/home/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.template.loader import get_template
 
 
 @login_required(login_url='/accounts/login')
 def home(request):
+    create_template()
     return render(request=request, template_name="home.html")
+
+
+def create_template():
+    shell = get_template("Entry.html")
+    with open(shell) as file:
+        print(file)
+
 
 
