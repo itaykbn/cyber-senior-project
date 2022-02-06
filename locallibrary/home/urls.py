1585525813fdd619c0216b7efdbeea910acb54bc
Itay Kabanovsky <itay.kbn@gmail.com>
2022-02-06 08:31:39 +0200

    post styling almost done

--- locallibrary/home/urls.py
+++ locallibrary/home/urls.py
@@ -6,9 +6,8 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-                  path('create/select/', TemplateView.as_view(template_name="create.html"), name='create'),
                   path("", views.home, name="home"),
                   path("create/select/", views.select, name="create"),
-                  #path("create/style/", views.style, name="style"),
+                  path("create/style/", views.style, name="style"),
                   path("messenger/", views.messenger, name="messenger")
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
